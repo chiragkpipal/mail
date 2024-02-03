312ec41fe593bf0a090fea760aaec9d379049451
Chirag Kumar <114102441+chiragkpipal@users.noreply.github.com>
2024-02-04 02:55:09 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+include('smtp/PHPMailerAutoload.php');
+
+$email = "chiragpipal@outlook.com";
+$sub = "hello";
+$message = 'Message'; 
+
+echo smtp_mailer($email,$sub,$message);
+function smtp_mailer($to,$subject, $msg){
+	$mail = new PHPMailer(); 
+	$mail->IsSMTP(); 
+	$mail->SMTPAuth = true; 
+	$mail->SMTPSecure = 'tls'; 
+	$mail->Host = "smtp.mailgun.org";
+	$mail->Port = 587; 
+	$mail->IsHTML(true);
+	$mail->CharSet = 'UTF-8';
+	//$mail->SMTPDebug = 2; 
+	$mail->Username = "chirag@ep360.me";
+	$mail->Password = "Kavya.99";
+	$mail->SetFrom("chirag@ep360.me");
+	$mail->Subject = $subject;
+	$mail->Body =$msg;
+	$mail->AddAddress($to);
+	$mail->SMTPOptions=array('ssl'=>array(
+		'verify_peer'=>false,
+		'verify_peer_name'=>false,
+		'allow_self_signed'=>false
+	));
+	if(!$mail->Send()){
+		echo $mail->ErrorInfo;
+	}else{
+		return 'Sent';
+	}
+}
+
+?>
